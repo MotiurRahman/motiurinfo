3d6f863ca45c74fa5dd54e7c452caf122a33195b
user <user@users-MacBook-Pro-3.local>
2016-12-27 03:13:46 +0600

    27/12

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var mime = require('mime');
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,20 +9,30 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/download', function(req, res,next) {
-    var file = '/cvFile/Resume_Of_Motiur.pdf';
-    console.log("File:" + file);
- 
-    res.download("/Resume_Of_Motiur.pdf", 'Resume_Of_Motiur.pdf', function(err) {
+router.get('/download_CV', function(req, res) {
+
+    var filePath = __dirname + '/cvFile/Resume_Of_Motiur.pdf';
+    var filePathor = '/Resume_Of_Motiur.pdf';
+
+    var fileName = path.basename(filePath);
+
+    console.log("BaseName:" + fileName);
+    console.log("FilePath:" + filePath);
+
+    res.download(filePathor, "Resume_Of_Motiur.pdf", function(err) {
+
+        res.set("Content-Disposition', 'attachment; filename=Resume_Of_Motiur.pdf");
+        res.set('Content-type', 'application/pdf');
+
         if (err) {
             // Handle error, but keep in mind the response may be partially-sent
             // so check res.headersSent
-             console.log("Failed");
-             res.redirect('/');
-             
-             
+            console.log(err);
+            res.redirect('/');
+
+
         } else {
-        	 console.log("Success");
+            console.log("Success");
             res.redirect('/');
         }
     }); // Set disposition and send it.
