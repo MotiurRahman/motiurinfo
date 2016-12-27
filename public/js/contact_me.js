20d8fb77ecead126172594b61ab2745a80884287
user <user@users-MacBook-Pro-3.local>
2016-12-28 04:01:20 +0600

    28/12

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -1,73 +1,103 @@
-$(function() {
+$(document).ready(function() {
+    $("#sendMes").hide();
+    $("#submitButton").click(function() {
 
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            event.preventDefault();
-            
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
+
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var phone = $("#phone").val();
+        var message = $("#message").val();
+
+        var contant = {
+            "name": name,
+            "email": email,
+            "phone": phone,
+            "message": message
+        };
+        if (name == "") {
+            $("#name").focus();
+        } else if (email == "") {
+            $("#email").focus();
+        } else if (phone == "") {
+            $("#phone").focus();
+        } else if (message == "") {
+            $("#message").focus();
+        } else {
+
+            $("#sendMes").show();
+            $("#submitButton").hide();
+
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+                    url: '/',
+                    type: 'POST',
+                    dataType: 'json',
+                    //data: $("#email").val(),
+                    data: JSON.stringify(contant),
+                    headers: {
+                        'Content-Type': 'application/json;charset=utf-8'
+                    },
+                    success: function(data, textStatus, jQxhr) {
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            });
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
+                        // alert("success:" + data);
+                        $("#sendMes").hide();
+                        $("#submitButton").show();
+
+
+                        if (data == "Message has been send successfully") {
+
+                            $("#submitButton").attr("disabled", false);
+                            $('#success').html("<div class='alert alert-success'>");
+                            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                .append("</button>");
+                            $('#success > .alert-success')
+                                .append("<strong>Your message has been sent. </strong>");
+                            $('#success > .alert-success')
+                                .append('</div>');
+
+                            //clear all fields
+                            $('#contactForm').trigger("reset");
+
+                        } else {
+
+                            $('#success').html("<div class='alert alert-danger'>");
+                            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                .append("</button>");
+                            $('#success > .alert-danger').append("<strong>Sorry, it seems your mail is not valid. Please try again later!");
+                            $('#success > .alert-danger').append('</div>');
+                            //clear all fields
+                            // $('#contactForm').trigger("reset");
+
+                        }
+
+
+
+
+
+
+                    },
+                    error: function(jqXhr, textStatus, errorThrown) {
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding. Please try again later!");
+                        $('#success > .alert-danger').append('</div>');
+                        //clear all fields
+                        // $('#contactForm').trigger("reset");
+                    }
+                })
+                .done(function(reuslt) {
+
+
+                })
+                .fail(function() {
+                    console.log("error");
+                })
+                .always(function() {
+                    console.log("complete");
+                });
+        }
+
     });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
 
-// When clicking on Full hide fail/success boxes
-$('#name').focus(function() {
-    $('#success').html('');
 });
--- routes/index.js
+++ routes/index.js
@@ -2,40 +2,90 @@ var express = require('express');
 var router = express.Router();
 var mime = require('mime');
 var path = require('path');
+var nodemailer = require('nodemailer');
+var validator = require("email-validator");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index');
-});
+}).post('/', function(req, res, next) {
+    var Name = req.body.name;
+    var Email = req.body.email;
+    var Phone = req.body.phone;
+    var Message = req.body.message;
 
+    console.log("nameValue:" + Name);
+    console.log("email:" + Email);
+    console.log("phone:" + Phone);
+    console.log("message:" + Message);
 
-router.get('/download_CV', function(req, res) {
-
-    var filePath = __dirname + '/cvFile/Resume_Of_Motiur.pdf';
-    var filePathor = '/Resume_Of_Motiur.pdf';
-
-    var fileName = path.basename(filePath);
-
-    console.log("BaseName:" + fileName);
-    console.log("FilePath:" + filePath);
-
-    res.download(filePathor, "Resume_Of_Motiur.pdf", function(err) {
-
-        res.set("Content-Disposition', 'attachment; filename=Resume_Of_Motiur.pdf");
-        res.set('Content-type', 'application/pdf');
-
-        if (err) {
-            // Handle error, but keep in mind the response may be partially-sent
-            // so check res.headersSent
-            console.log(err);
-            res.redirect('/');
-
-
-        } else {
-            console.log("Success");
-            res.redirect('/');
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+            user: 'motiur.mbstu@gmail.com', // Your email id
+            pass: 'ugtsqqbrdyabdvpg' // Your password
         }
-    }); // Set disposition and send it.
+    });
+
+
+    var mailOptions = {
+        from: Email, // sender address
+        to: 'motiur.mbstu@gmail.com', // list of receivers
+        subject: Name, // Subject line
+        text: Message //, // plaintext body
+            // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+    };
+
+    if (validator.validate(Email)) {
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+                res.json("Please try again");
+            } else {
+                console.log('Message sent: ' + info.response);
+                res.json("Message has been send successfully");
+
+                //res.redirect("/contact");
+
+            };
+        });
+
+
+    } else {
+        res.json("Please try again with valid email");
+    }
+
 });
+
+
+// router.get('/download_CV', function(req, res) {
+
+//     var filePath = __dirname + '/cvFile/Resume_Of_Motiur.pdf';
+//     var filePathor = '/Resume_Of_Motiur.pdf';
+
+//     var fileName = path.basename(filePath);
+
+//     console.log("BaseName:" + fileName);
+//     console.log("FilePath:" + filePath);
+
+//     res.download(filePathor, "Resume_Of_Motiur.pdf", function(err) {
+
+//         res.set("Content-Disposition', 'attachment; filename=Resume_Of_Motiur.pdf");
+//         res.set('Content-type', 'application/pdf');
+
+//         if (err) {
+//             // Handle error, but keep in mind the response may be partially-sent
+//             // so check res.headersSent
+//             console.log(err);
+//             res.redirect('/');
+
+
+//         } else {
+//             console.log("Success");
+//             res.redirect('/');
+//         }
+//     }); // Set disposition and send it.
+// });
 
 module.exports = router;
